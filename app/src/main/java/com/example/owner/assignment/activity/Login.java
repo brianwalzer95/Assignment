30a731ca220f3913b7a0e47cb99c2e8f9c5ff2ad
Brian Walzer <brianwalzer95@hotmail.com>
2018-03-28 17:25:27 +0100

    Assignment

---
+++ app/src/main/java/com/example/owner/assignment/activity/Login.java
@@ -0,0 +1,15 @@
+package com.example.owner.assignment.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.owner.assignment.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/owner/assignment/entity/Book.java
@@ -0,0 +1,9 @@
+package com.example.owner.assignment.entity;
+
+/**
+ * Created by Owner on 28/03/2018.
+ */
+
+public class Book {
+
+}
